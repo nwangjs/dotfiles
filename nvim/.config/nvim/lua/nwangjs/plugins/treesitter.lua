cc4ce08b98b3bd338d202f75b0e2d4f5adf9d98f
Nathan Wang <nwangjs@gmail.com>
2025-10-14 18:14:29 -0500

    Fix lspconfig lazy loading

--- nvim/.config/nvim/lua/nwangjs/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/nwangjs/plugins/treesitter.lua
@@ -2,12 +2,12 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    event = "BufReadPost",
+    event = { "BufReadPost", "BufNewFile" },
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'cpp', 'python', 'bash', 'lua' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -15,9 +15,8 @@ return {
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true, disable = { } },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
