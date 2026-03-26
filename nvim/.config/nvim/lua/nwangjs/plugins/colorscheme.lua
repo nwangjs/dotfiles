e402bae7ce6d132781005cf5bc08935dc9f8968c
Nathan Wang <nwangjs@gmail.com>
2026-03-25 22:06:12 -0700

    Add TokyoNight colorscheme

---
+++ nvim/.config/nvim/lua/nwangjs/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+-- Colorscheme
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup()
+      vim.cmd.colorscheme("tokyonight-night")
+    end,
+  },
+}
