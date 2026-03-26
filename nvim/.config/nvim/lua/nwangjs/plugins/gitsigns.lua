cf9a315869b1119445fa34d4d556921c04aa0565
Nathan Wang <nwangjs@gmail.com>
2026-03-26 11:44:02 -0700

    Add vim-fugitive and gitsigns.nvim

---
+++ nvim/.config/nvim/lua/nwangjs/plugins/gitsigns.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    'lewis6991/gitsigns.nvim',
+    event = { 'BufReadPost', 'BufNewFile' },
+    opts = {
+      signs = {
+        add          = { text = '┃' },
+        change       = { text = '┃' },
+        delete       = { text = '_' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked    = { text = '┆' },
+      },
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- TODO: Add keymaps
+
+      end,
+    },
+  },
+}
